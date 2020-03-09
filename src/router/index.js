6a982d9959f7937469192a0da2cb1ea7308d20ed
SJJ <sunjiajunboy@gmail.com>
2020-03-09 09:03:51 +0800

    添加yarn.lock

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/HomeOld.vue'
+import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,54 @@
 <template>
   <v-app>
+      <v-row>
+        <v-navigation-drawer
+          permanent
+          
+        >
+          <v-text-field
+            outlined
+            rounded
+            hide-details
+            single-line
+            clearable
+            prepend-inner-icon="mdi-magnify"
+            label="Search"
+            class="pa-2"
+          />
+          <v-list
+            three-line
+          >
+            <!--        <v-subheader>today</v-subheader>-->
+            <v-list-item @click="iconClick1">
+              <v-list-item-avatar>
+                <v-img src="https://cdn.vuetifyjs.com/images/lists/1.jpg"/>
+              </v-list-item-avatar>
+              <v-list-item-content>
+                <v-list-item-title>Travis Howard</v-list-item-title>
+              </v-list-item-content>
+              <!--            <v-list-item-content>-->
+              <!--              <v-list-item-title>Travis Howard</v-list-item-title>-->
+              <!--              <v-list-item-subtitle><span class='text&#45;&#45;primary'>Ali Connors</span> &mdash; I'll be in your-->
+              <!--                neighborhood-->
+              <!--                doing errands this weekend. Do you want to hang out?-->
+              <!--              </v-list-item-subtitle>-->
+              <!--            </v-list-item-content>-->
+            </v-list-item>
+          </v-list>
+        </v-navigation-drawer>
+        <v-app-bar elevation="1" color="white">
+          <v-toolbar-title>Bncfbb</v-toolbar-title>
 
+          <v-spacer/>
+
+          <v-btn icon>
+            <v-icon>mdi-magnify</v-icon>
+          </v-btn>
+          <v-btn icon>
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </v-app-bar>
+      </v-row>
   </v-app>
 </template>
 
